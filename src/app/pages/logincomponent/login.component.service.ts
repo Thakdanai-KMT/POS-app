423f24611982d803c2b856e180cc082db9403007
Fikkung <study0001fix@gmail.com>
2025-12-23 20:32:33 +0700

    fix: update API URL in LoginService to correct endpoint

--- src/app/pages/logincomponent/login.component.service.ts
+++ src/app/pages/logincomponent/login.component.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LoginService {
 
-  private apiUrl = 'http://localhost:3000/api/auth/login';
+  private apiUrl = 'http://localhost:10000/api/auth/login';
 
   constructor(private http: HttpClient) {}
 
