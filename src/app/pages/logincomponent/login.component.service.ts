ca1422be9b8ba7824cfeb449ecc65a3fff147162
Fikkung <study0001fix@gmail.com>
2025-12-23 20:56:02 +0700

    fix: correct API URL in LoginService by removing duplicate protocol

--- src/app/pages/logincomponent/login.component.service.ts
+++ src/app/pages/logincomponent/login.component.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LoginService {
 
-  private apiUrl = 'http://https://backend-oczg.onrender.com/api/auth/login';
+  private apiUrl = 'https://backend-oczg.onrender.com/api/auth/login';
   // private apiUrl = 'http://localhost:10000/api/auth/login';
 
   constructor(private http: HttpClient) {}
